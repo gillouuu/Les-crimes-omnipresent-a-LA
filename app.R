13623d5ee2b271df3b57efb5e9a202ac558386ad
corentin gilles <corentingilles81@gmail.com>
2023-12-30 20:12:42 +0100

    commit 30/12 20h12

--- app.R
+++ app.R
@@ -8,7 +8,7 @@ ui <- fluidPage(
       sidebarPanel(width = 3,
                    h3("Sélectionnez la victime"),
                    h5("Sélectionnez le sexe"),
-                   checkboxGroupInput("sexe", "", choices = c("Homme","Femme")),
+                   checkboxGroupInput("sexe", "", choices = c("Homme"="M","Femme"="F","Autre"="X")),
                    br(),
                    br(),
                    h5("Sélectionnez l'âge"),
@@ -22,25 +22,37 @@ ui <- fluidPage(
       
       mainPanel(
         h4("Nombre total de crimes commis sur la victime en question:"),
-        textOutput("totalCrimesText")
+        # Utilisation d'un KPI
+        infoBox("Total de crimes", value = textOutput("totalCrimesText")),
+        textOutput("selectedFilters"),
+        textOutput("uniqueSexValues"),
+        textOutput("filteredDataCount"),
+        textOutput("percentageText")
       )
     )
   )
 )
 
 server <- function(input, output) {
+  filteredData <- reactive({
+    subset(data,
+           data$`Vict.Sex` %in% input$sexe &
+             data$`Vict.Age` >= input$age[1] & data$`Vict.Age` <= input$age[2] &
+             data$`Vict.Age` != -2 &
+             data$`Vict.Sex` != "H")
+  })
+  
   output$totalCrimesText <- renderText({
-    # Filtrer le jeu de données en fonction des filtres sélectionnés
-    filteredData <- subset(data,
-                           "Vict Sex" %in% input$sexe &
-                             "Vict Age" >= input$age[1] & "Vict Age" <= input$age[2])
-    
-    
-    # Calculer le nombre total de crimes
-    totalCrimes <- nrow(filteredData)
-    
-    # Retourner le texte à afficher
+    totalCrimes <- nrow(filteredData())
     paste(totalCrimes)
+  })
+  
+ 
+  output$percentageText <- renderText({
+    totalCrimes <- nrow(filteredData())
+    totalRowsFiltered <- nrow(data[data$`Vict.Age` != -2 & data$`Vict.Sex` != "H",])
+    percentage <- (totalCrimes / totalRowsFiltered) * 100
+    paste("Pourcentage des crimes sélectionnés par rapport au total :", round(percentage, 2), "%")
   })
 }
 
