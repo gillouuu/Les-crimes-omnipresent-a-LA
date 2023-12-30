f439b946f09d8a36dc8ffb5f5a1b67b576848bb3
corentin gilles <corentingilles81@gmail.com>
2023-12-30 14:27:39 +0100

    30/12 14h27

--- app.R
+++ app.R
@@ -1,40 +1,47 @@
-source(file="global.R")
+source(file = "global.R")
 source(file = "Packages.R")
 
-
-ui<-fluidPage(
+ui <- fluidPage(
   div(
-    h1("Les crimes à LA en fonction des victimes",align="center"),
+    h1("Les crimes à LA en fonction des victimes", align = "center"),
     sidebarLayout(
       sidebarPanel(width = 3,
-                   h5("Sélectionnez la victime"),
-                   checkboxGroupInput("box_group","", choices = c("Homme","Femme")),
-                   h5("Sélectionnez l'âge de la victime"),
-                   sliderInput("slider","",0,100,c(25,75)),
-                   h5("Sélectionnez l'ethnie de la victime"),
-                   checkboxGroupInput("box_group","", choices = c("Latinos","Blancs","Noirs","Asiatiques","Autres","Non spécifié")),
+                   h3("Sélectionnez la victime"),
+                   h5("Sélectionnez le sexe"),
+                   checkboxGroupInput("sexe", "", choices = c("Homme","Femme")),
+                   br(),
+                   br(),
+                   h5("Sélectionnez l'âge"),
+                   sliderInput("age","",0,100,c(25,75)),
+                   br(),
+                   br(),
+                   h5("Sélectionnez l'ethnie"),
+                   checkboxGroupInput("ethnie","", choices = c("Latinos","Blancs","Noirs","Asiatiques","Autres","Non spécifié")),
                    
       ),
+      
       mainPanel(
-        
+        h4("Nombre total de crimes commis sur la victime en question:"),
+        textOutput("totalCrimesText")
       )
-    )))
-
-
-
-
-
-
-
-
-
-
-
-
+    )
+  )
+)
 
 server <- function(input, output) {
-  
+  output$totalCrimesText <- renderText({
+    # Filtrer le jeu de données en fonction des filtres sélectionnés
+    filteredData <- subset(data,
+                           "Vict Sex" %in% input$sexe &
+                             "Vict Age" >= input$age[1] & "Vict Age" <= input$age[2])
+    
+    
+    # Calculer le nombre total de crimes
+    totalCrimes <- nrow(filteredData)
+    
+    # Retourner le texte à afficher
+    paste(totalCrimes)
+  })
 }
 
-shinyApp(ui=ui , server=server)
-
+shinyApp(ui = ui, server = server)
